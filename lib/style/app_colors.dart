8bb62eed7b3aab31a70af9c337c7f465bce9cf56
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2026-03-28 13:45:00 +0100

    Add App Styles

---
+++ lib/style/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color textPrimary = Color(0xFF000000);
+  static const Color textSecondary = Color(0xFFFFFFFF);
+  static const Color backgroundPrimary = Color(0xFFFFFFFF);
+  static const Color buttonSecondary = Color(0xFFFFFFFF);
+  static const Color borderDefault = Color(0xFFF2F4F6);
+  static const Color textAdditional = Color(0xFFCDCDCD);
+}
---
+++ lib/style/style.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'app_colors.dart';
+
+class Style {
+  static final whiteTheme = ThemeData(
+    //todo fontFamily: 'use-demo-font',
+    scaffoldBackgroundColor: AppColors.backgroundPrimary,
+    appBarTheme: AppBarTheme(
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarIconBrightness: Brightness.dark,
+        statusBarBrightness: Brightness.light,
+      ),
+      backgroundColor: AppColors.backgroundPrimary,
+      foregroundColor: AppColors.textPrimary,
+    ),
+  );
+
+  static final text26w700PrimaryStyle = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.w700,
+    //todo fontFamily: 'Bold',
+    height: 1.2,
+    color: AppColors.textPrimary,
+  );
+
+  static final text22w700PrimaryStyle = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.w700,
+    //todo fontFamily: 'Bold',
+    height: 1.2,
+    color: AppColors.textPrimary,
+  );
+
+  static final text16w600SecondaryStyle = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+    //todo fontFamily: 'Demibold',
+    color: AppColors.textSecondary,
+  );
+
+  static final text16w600PrimaryStyle = text16w600SecondaryStyle.copyWith(
+    //todo fontFamily: 'Demibold',
+    height: 1.35,
+    color: AppColors.textPrimary,
+  );
+
+  static final text14w600SecondaryStyle = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    //todo fontFamily: 'Demibold',
+    height: 1.2,
+    color: AppColors.textSecondary,
+  );
+
+  static final text14w600PrimaryStyle = text14w600SecondaryStyle.copyWith(color: AppColors.textPrimary, height: 1.1);
+
+  static final text12w600PrimaryStyle = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.w600,
+    //todo fontFamily: 'Demibold',
+    color: AppColors.textPrimary,
+  );
+
+  static final text12w700SecondaryStyle = text12w600PrimaryStyle.copyWith(
+    fontWeight: FontWeight.w700,
+    height: 1.4,
+    //todo fontFamily: 'Bold',
+    color: AppColors.textSecondary,
+  );
+
+  static final text10NormalSecondaryStyle = TextStyle(
+    fontSize: 10,
+    fontWeight: FontWeight.w400,
+    color: AppColors.textSecondary,
+  );
+}
